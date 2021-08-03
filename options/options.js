471c7c8e2cebf73de435edbe58126d8d8d7da4c5
Crowdbotics <team@crowdbotics.com>
2021-08-03 15:08:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "billowing_bread_29402Identifier",
+  url: "https://billowing_bread_29402Identifier.botics.co",
+  api: "https://billowing_bread_29402Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
